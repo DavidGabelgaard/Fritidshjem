e18787d4fa4556151cb9f86bf20ab2ad83af87ac
DavidGabelgaard <89967408+DavidGabelgaard@users.noreply.github.com>
2021-11-09 14:37:34 +0100

    Initial commit

---
+++ src/com/company/VareData.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class VareData implements java.io.Serializable {
+
+    int antal;
+    String vareNavn;
+    double stykPris;
+
+
+
+    public VareData(int antal, String vareNavn , double stykPris) {
+        this.antal = antal;
+        this.vareNavn = vareNavn;
+        this.stykPris = stykPris;
+    }
+
+    public int getAntal() { return antal;}
+    public void setAntal(int antal) { this.antal = antal; }
+
+    public String getVareNavn() { return vareNavn; }
+    public void setVareNavn(String vareNavn) { this.vareNavn = vareNavn; }
+
+    public double getStykPris() { return stykPris; }
+    public void setStykPris(double stykPris) { this.stykPris = stykPris; }
+
+
+
+
+}
